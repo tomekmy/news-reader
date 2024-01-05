9dd2c6c5aa20a502b3a48eb4ce713954f81d4c0d
Tomasz Myśliwiec <tomasz.mysliwiec@execon.pl>
2024-01-05 13:28:16 +0100

    chore: prepare project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import moment from "moment";
 import parse from "rss-to-json";
-import './App.css'
 import dataSources from './utils/data-sources';
+import Menu from './components/Menu/Menu';
+
+type FeedItem = {
+  title: string;
+  created: string;
+  description: string;
+  link: string;
+};
+
+type Feed = {
+  title: string;
+  description: string;
+  link: string;
+  items: FeedItem[];
+};
+
 
 function App() {
-  const [data, setData] = useState<any>([]);
+  const [feed, setFeed] = useState<Feed[] | null>(null);
 
   useEffect(() => {
     const promisees = dataSources[0].sources.map((source) => parse(source.url));
     Promise.all(promisees).then((values) => {
       console.log(values);
-      setData(values);
+      setFeed(values as unknown as Feed[]);
     });
   }, []);
 
 
   return (
-    <>
-      {JSON.stringify(data)}
-    </>
+    <div className="min-h-screen min-w-full p-6 bg-white dark:bg-slate-800 dark:text-white">
+      <Menu />
+      <main>
+        {dataSources.map((source) => (
+          <div key={source.sourceName} style={{backgroundColor: source.darkColor}}>
+            <div className="p-5 text-center grid justify-items-center	gap-3">
+              <h1 className="font-bold text-lg">{source.sourceName}</h1>
+              <img src={`/logos/${source.logoFileName}`} alt="News feed logo" className='h-12'/>
+            </div>
+            <div className="flex flex-wrap gap-4 justify-center p-5 items-start">
+            {
+              feed?.map((source) => (
+                <div key={source.title} className="grid gap-1 max-w-96 min-w-60 pt-4 items-start">
+                  <div className="font-semibold">Nazwa kanału: {source.title}</div>
+                  <div className="font-semibold">Opis kanału: {source.description}</div>
+                  <div className="text-sm">Źródło: <a className="hover:text-slate-300" href={source.link}>{source.link}</a></div>
+                  {
+                  source.items.map((item) => (
+                    <div key={item.title} className="grid gap-2 max-w-96 min-w-60 pt-4 items-start">
+                      <h3 className="font-bold">{item.title}</h3>
+                      <p className="text-sm">{moment(item.created).format('DD/MM/YYYY hh:mm')}</p>
+                      <p className="indent-2">{item.description}</p>
+                      <a className="text-sm hover:text-slate-300" href={item.link}>Czytaj artykuł</a>
+                    </div>
+                  ))}
+                </div>
+              ))
+            }
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,46 @@
+"use client";
+import dataSources from "../../utils/data-sources";
+
+const Menu = () => {
+  const sources = localStorage.getItem("sources") || '';
+  
+  const handleCheckboxChange = (event) => {
+    console.log('event', event);
+  };
+
+  return (
+    <nav className="sticky bg-slate-200 text-black w-full p-2 border-solid border-2 border-sky-500">
+      <ul>
+        {dataSources.map(item => (
+          <li key={item.sourceName}>
+            {item.sourceName}
+            {!!item.sources.length && (
+              <ul className="pl-3 pb-2">
+                {item.sources.map(source => (
+                  <li key={`${item.sourceName}_${source.name}`}>
+                    <input
+                      className="cursor-pointer accent-neutral-800"
+                      type="checkbox" id={`${item.sourceName}_${source.name}`}
+                      name={`${item.sourceName}_${source.name}`}
+                      value={source.name}
+                      checked={!sources}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label
+                      className="cursor-pointer pl-2"
+                      htmlFor={`${item.sourceName}_${source.name}`}
+                    >
+                      {source.name}
+                    </label>
+                  </li>
+                ))}
+              </ul>)}
+          </li>
+
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
